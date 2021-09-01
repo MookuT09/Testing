1dc195fd72d585bb19160461e7be2241966b699f
mouax240 <mouax240@umn.edu>
2021-09-01 14:44:40 -0500

    1st push

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,18 @@
+//Testing
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("validationInputValidation.controller.App", {
+		onCreate: function () {
+			console.log("Working");
+			// send new item to server for processing
+			//this.getModel().submitChanges();
+			
+			
+			
+		}
+
+	});
+});
